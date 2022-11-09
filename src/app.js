8a93fc21db5826349a8758a39cb9901322dba619
Riv <rivenshell111@gmail.com>
2022-11-09 20:35:44 +0000

    Initial Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Random card generator
+
+  console.log("Random number");
 };
