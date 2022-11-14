3fac5bfe854fe4e3701ef0fbcad9e337d6482adf
Rivenshell <rivenshell111@gmail.com>
2022-11-13 22:54:00 -0500

    logic initialization

--- src/app.js
+++ src/app.js
@@ -8,6 +8,12 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   let number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   let suite = ["♦", "♥", "♠", "♣"];
+  let secondsuite = ["♦", "♥", "♠", "♣"];
+
+  //dimond
+  //heart
+  //spade
+  //club
 
   function shuffle(arr) {
     return arr[Math.floor(Math.random() * arr.length)];
@@ -15,12 +21,16 @@ window.onload = function() {
 
   let count = shuffle(number);
   let symbol = shuffle(suite);
+  let secondsuitesymbol = shuffle(secondsuite);
 
   let result = count;
   document.getElementById("logic").innerHTML = result;
 
   let newresult = symbol;
   document.getElementById("suitesymbol").innerHTML = newresult;
+
+  let thirdresult = secondsuitesymbol;
+  document.getElementById("secondsuitesymbol").innerHTML = newresult;
 
   console.log("Random number");
 };
